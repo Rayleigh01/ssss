d1ee35e27db91f2467dd1e0c6a3fc2d4eaf0ec7a
Rayleigh01 <90775310+Rayleigh01@users.noreply.github.com>
2021-09-15 21:57:04 +0530

    v1

---
+++ script.js
@@ -0,0 +1,55 @@
+const formName = document.querySelector('.user-form');
+        const loginButton = formName.querySelector('.login-button')
+        const pushButton = document.querySelector('.profile-push-button')
+        const eventButton = document.querySelector('.raise-event-button')
+
+
+        
+        
+        const submitLogin = (e) => {
+            e.preventDefault();
+            const username = formName.querySelector('.username-input').value;
+            const email = formName.querySelector('.email-input').value;
+            const phNumber = formName.querySelector('.phNumber-input').value;
+            const dob = formName.querySelector('.dob-input').value;
+            console.log(username, email, phNumber, dob);
+            
+            clevertap.onUserLogin.push({
+                "Site": {
+                    "Name": username,            // use the Name variable
+                    "Email": email,         // Email address of the user
+                    "Phone": phNumber,           // Phone (with the country code
+                    "DOB": new Date()                 // Date of Birth. Date object   
+                }
+            });
+         }
+        const submitPush = () => {
+
+            const username = formName.querySelector('.username-input').value;
+            const email = formName.querySelector('.email-input').value;
+            const phNumber = formName.querySelector('.phNumber-input').value;
+            const dob = formName.querySelector('.dob-input').value;
+
+            clevertap.profile.push({
+                "Site": {
+                  "Name": username,           // use the Name variable
+                  "Email": email,         // Email address of the user
+                  "Phone": phNumber,           // Phone (with the country code
+                  "DOB": new Date()                 // Date of Birth. Date object   
+                }
+               });
+
+        }
+        const submitEvent = () => {
+            const username = formName.querySelector('.username-input').value;
+            const email = formName.querySelector('.email-input').value;
+            const phNumber = formName.querySelector('.phNumber-input').value;
+            const dob = formName.querySelector('.dob-input').value;
+
+            
+        }
+ 
+ formName.addEventListener('submit', e => submitLogin(e) )
+ pushButton.addEventListener('click', submitPush )
+ eventButton.addEventListener('click', submitEvent )
+ 
